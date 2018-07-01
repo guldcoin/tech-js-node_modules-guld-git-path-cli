b3ae09e2467c2d612b2cdd0546dd29814019a23f
Ira Miller <public@iramiller.com>
2018-07-01 11:38:05 -0500

    v1.0.0

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
-# guld-user-cli
+# guld-git-path-cli
 
-Guld user management tools. Get, list, and check users of the guld group.
+Guld tool for getting the git directory of a path, or converting local paths into git-friendly repo names.
 
 ### Install
 
 ```
-npm i -g guld-user-cli
+npm i -g guld-git-path-cli
 ```
 
 ### Usage
@@ -13,20 +13,17 @@ npm i -g guld-user-cli
 ##### CLI
 
 ```
-  Usage: guld-user [options] [command]
+  Usage: guld-git-path [options] [command]
 
-  Guld user management tools. Get, list, and check users of the guld group.
+  Blocktree path resolution tools.
 
   Options:
 
-    -V, --version                 output the version number
-    -h, --help                    output usage information
+    -V, --version  output the version number
+    -h, --help     output usage information
 
   Commands:
 
-    init [user-name] [full-name]  Initialize a new guld user.
-    name                          Get the guld name of the current user.
-    fullname                      Get the full name of the current user.
-    exists                        Check whether a guld name already exists.
-    validate                      Check whether a guld name looks valid.
+    escape [path]  Convert a local path into an escaped blocktree path, like used in git repo names.
+    dir [path]     Guld tool for getting the git directory of a path.
 ```
--- cli.js
+++ cli.js
@@ -1,109 +1,25 @@
 #!/usr/bin/env node
-const { setupConfig } = require('guld-git-config')
-const { getName, getFullName, exists, validate } = require('guld-user')
-const inquirer = require('inquirer')
+const { getGitDir, pathEscape } = require('guld-git-path')
 const program = require('commander')
 const VERSION = require('./package.json').version
 
 /* eslint-disable no-console */
 program
-  .description('Guld user management tools. Get, list, and check users of the guld group.')
+  .name('guld-git-path')
+  .description('Blocktree path resolution tools.')
   .version(VERSION)
 program
-  .command('init [user-name] [full-name]')
-  .description('Initialize a new guld user.')
+  .command('escape [path]')
+  .description('Convert a local path into an escaped blocktree path, like used in git repo names.')
+  .action(async (p, options) => {
+    console.log(pathEscape(p))
+  })
 program
-  .command('name')
-  .description('Get the guld name of the current user.')
-program
-  .command('fullname')
-  .description('Get the full name of the current user.')
-program
-  .command('exists')
-  .description('Check whether a guld name already exists.')
-program
-  .command('validate')
-  .description('Check whether a guld name looks valid.')
-/*
-program
-  .command('register')
-  .option('-t,--type <ntype>', '', /^(individual|group|device|chain|ledger|address)$/i)
-  .description('Register a name of particular type')
-program
-  .command('registered')
-  .description('Check whether a guld name is registered.')
-*/
+  .command('dir [path]')
+  .description('Guld tool for getting the git directory of a path.')
+  .action(async (p, options) => {
+    console.log(getGitDir(p))
+  })
 
 program.parse(process.argv)
-
-var cmd
-if (program.commands.map(c => c._name).indexOf(program.args[0]) !== -1) cmd = program.args.shift()
-/* eslint-disable no-console */
-
-function inquireNames (user, full) {
-  inquirer
-    .prompt([
-      {
-        name: 'guldname',
-        type: 'input',
-        message: 'What is your individual guld name?',
-        default: user
-      },
-      {
-        name: 'fullname',
-        type: 'input',
-        message: 'What is your full name? (WARNING: public info!)',
-        default: full
-      }
-    ]).then(answers => {
-      answers.fullname = answers.fullname || answers.guldname
-      setupConfig({
-        'user': {
-          'username': answers.guldname,
-          'name': answers.fullname
-        }
-      })
-    })
-}
-
-async function checkName (fn, n) {
-  try {
-    if (await fn(n)) console.log(true)
-    else {
-      console.log(false)
-      process.exit(1)
-    }
-  } catch (e) {
-    console.error(e.message)
-    process.exit(1)
-  }
-}
-
-switch (cmd) {
-  case 'init':
-    if (program.args.length > 0) inquireNames(...program.args)
-    else {
-      getName().then(user => {
-        getFullName().then(full => {
-          inquireNames(user, full)
-        })
-      })
-    }
-    break
-  case 'exists':
-    if (program.args.length > 0) checkName(exists, program.args[0])
-    else getName().then(n => checkName(exists, n))
-    break
-  case 'validate':
-    if (program.args.length > 0) checkName(validate, program.args[0])
-    else getName().then(n => checkName(validate, n))
-    break
-  case 'fullname':
-    getFullName().then(console.log)
-    break
-  case 'name':
-  default:
-    getName().then(console.log)
-    break
-}
-/* eslint-enable no-console */
+if (program.args.length === 0) console.log(pathEscape())
